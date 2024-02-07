468679e6cfafddc7ce3d1232175dc336dcf43ff8
Rashmi <rashmipc@qliclabs.com>
2024-02-07 18:53:27 +0530

    done

---
+++ ResuseApplication/src/main/java/com/example/resuseapplication/testForm/testForm.kt
@@ -0,0 +1,12 @@
+package com.example.resuseapplication.testForm
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.resuseapplication.R
+
+class TestFromActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_from)
+    }
+}
